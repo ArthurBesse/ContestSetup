e196a97d98ae5ebba10128c1b0b120a4e5b689ee
Arthur <arthurcaturian@gmail.com>
2022-10-18 17:31:58 +0400

    Update main.cpp

--- ContestSetup/main.cpp
+++ ContestSetup/main.cpp
@@ -1098,56 +1098,21 @@ namespace abesse
 		std::vector<std::map<char, int>> edges; // edges[i]  : the labeled edges from node i
 		std::vector<int> link;            // link[i]   : the parent of i
 		std::vector<int> length;          // length[i] : the length of the longest string in the ith class
-		std::string cur_str;
 		int last;                    // the index of the equivalence class of the whole string
 		int count;
+		int number_of_distinct_substrings;
 
 		SuffixAutomaton(std::string const& s)
-			: cur_str(s)
-			, last(0)
+			: last(0)
 			, count(0)
+			, number_of_distinct_substrings(0)
 		{
 			// add the initial node
 			edges.push_back(std::map<char, int>());
 			link.push_back(-1);
 			length.push_back(0);
-			for (; count < s.size(); count++) {
-				// construct r
-				edges.emplace_back();
-				length.push_back(count + 1);
-				link.push_back(0);
-				int r = static_cast<int>(edges.size()) - 1;
-
-				// add edges to r and find p with link to q
-				int p = last;
-				while (p >= 0 && edges[p].find(s[count]) == edges[p].end()) {
-					edges[p][s[count]] = r;
-					p = link[p];
-				}
-				if (p != -1) {
-					int q = edges[p][s[count]];
-					if (length[p] + 1 == length[q]) {
-						// we do not have to split q, just set the correct suffix link
-						link[r] = q;
-					}
-					else {
-						// we have to split, add q'
-						edges.push_back(edges[q]); // copy edges of q
-						length.push_back(length[p] + 1);
-						link.push_back(link[q]); // copy parent of q
-						int qq = static_cast<int>(edges.size()) - 1;
-						// add qq as the new parent of q and r
-						link[q] = qq;
-						link[r] = qq;
-						// move short classes pointing to q to point to q'
-						while (p >= 0 && edges[p][s[count]] == q) {
-							edges[p][s[count]] = qq;
-							p = link[p];
-						}
-					}
-				}
-				last = r;
-			}
+			for (size_t i = 0; i < s.size(); i++)
+				extend(s[i]);
 		}
 
 		void extend(char c)
@@ -1186,7 +1151,7 @@ namespace abesse
 				}
 			}
 			last = r;
-			cur_str.push_back(c);
+			number_of_distinct_substrings += length[last] - length[link[last]];
 		}
 
 		std::string lcs(std::string const& str)
@@ -1221,6 +1186,11 @@ namespace abesse
 				n = edges[n][str[i]];
 			}
 			return !fail;
+		}
+
+		int get_number_of_distinct_substrings()
+		{
+			return number_of_distinct_substrings;
 		}
 	};
 
